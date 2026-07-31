bd246bb36c76c1242a03ec60ad1cafbd5779a8b6
Balasubramanian Shanmugham <bala.subramanian0027@gmail.com>
2026-07-31 14:21:53 +0530

    docs(architecture): add Architecture concepts page (#31) (#32)

--- tome.config.js
+++ tome.config.js
@@ -37,7 +37,7 @@ export default {
     },
     {
       group: "Concepts",
-      pages: ["concepts/core-concepts"],
+      pages: ["concepts/core-concepts", "concepts/architecture"],
     },
   ],
   socialLinks: [
